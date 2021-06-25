9a8b7c85ec3c9252bc06cb20a664b42fa96e368e
Sarath-97 <sarath559709@gmail.com>
2021-06-25 15:31:10 +0100

    more improvements

--- index.js
+++ index.js
@@ -1,24 +1,80 @@
-window.onload = () => {
-    fetchBooks()
-}
+let array = []
+        window.onload = () => {
+        //    fetchCards()
+           
+        
+        }
 
-const fetchBooks = () =>{
-    const row = document.getElementById("row");
-    fetch(' https://striveschool-api.herokuapp.com/books')
-    .then(response => response.json())
-    .then(data => {
-    console.log(data)
- 
-    data.forEach(book => {
+        const fetchBooks = () => {
+            fetch("https://striveschool-api.herokuapp.com/books")
+            .then(response => response.json())
+            .then(books => {
+                console.log(books)
 
-    const card = ` <div class="card" style="width: 18rem;">
-    <img src="${book.img}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-  </div>`
-    row.innerHTML += card
-})
-    })
+                array = books
+            } )
+        }
+        
+        
+        fetchBooks()
+        
+        const show = () => fetchCards()
+        
+        const fetchCards = () => {
+            
+            const row = document.getElementById("row");
+           
+            
+                array.forEach(book => {
+                    const card = `<div class=" hide col-md-4">
+                        <div  class="card mb-4 shadow-sm">
+                            
+                <img src=${book.img}
+                  <title>${book.title}</title>
+                
+                  </text>
+                
+                <div class="card-body">
+                    
+                  <p class="card-text">
+                    ${book.category}
+                  </p>
+                  <span badge bg-warning text-dark mb-2"> Price: € ${book.price}</span>
+                 
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <div class=" addToCart btn-group">
+                      <button onlick = "addToCard()"
+                        type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                        Add to cart
+                      </button>
+                      <button onclick ="hideMe(event)" type="button"
+                        class="btn btn-sm btn-outline-secondary"
+                      >
+                       Hide
+                      </button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>`
+                        row.innerHTML += card
 
-}
+
+
+                    })
+
+
+
+                
+            }
+            
+            const hideMe = (event) => {
+        event.target.closest(".hide").remove();
+    }
+
+
